f0b31faccde8b990de942801017174ed6582e33e
Arisa Miyasaka <11729510+arisamiyasaka@users.noreply.github.com>
2017-11-10 23:37:38 +0900

    redesign source process

--- src/App.js
+++ src/App.js
@@ -19,20 +19,18 @@ class App extends Component {
   }
 
   toggleTask(state, id) {
-    const { tasks } = state
-    const newTasks = tasks.map(task => {
+    const tasks = state.tasks.map(task => {
       if (task.id !== id) return { ...task }
       return { ...task, completed: !task.completed }
     })
-    const newState = { ...state, tasks: newTasks }
+    const newState = { ...state, tasks }
     localStorage.setItem('myTodo', JSON.stringify(newState))
     return newState
   }
 
-  addTask(description = 'New Task!') {
-    const { tasks } = this.state
-    const newTasks = [...tasks, { id: v4(), completed: false, description: description }]
-    const newState = { ...this.state, tasks: newTasks }
+  addTask(description) {
+    const tasks = [...this.state.tasks, { id: v4(), completed: false, description }]
+    const newState = { ...this.state, tasks }
     localStorage.setItem('myTodo', JSON.stringify(newState))
 
     this.setState(newState)
@@ -40,26 +38,20 @@ class App extends Component {
 
   render() {
     const { tasks } = this.state
-    const that = this
     return (
       <div>
-        <MyForm myEvent={(desc) => this.addTask(desc)} />
+        <MyForm myEvent={desc => this.addTask(desc)} />
 
         <ul>
-          {tasks.map(function (task) {　/* JS ここから */
-            /* React ここから */
-            return (
-              <li key={task.id} style={{
-                textDecoration: task.completed ? 'line-through' : 'none'
-              }}
-                onClick={() => {
-                  that.setState(that.toggleTask(that.state, task.id))
-                }
-                }>
-                {task.description}
-              </li>
-            )
-          })}
+          {tasks.map(({ id, completed, description }) => (
+            <li
+              key={id}
+              style={{ textDecoration: completed ? 'line-through' : 'none' }}
+              onClick={() => this.setState(this.toggleTask(this.state, id))}
+            >
+              {description}
+            </li>
+          ))}
         </ul>
       </div>
     );
