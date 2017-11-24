17c504d4cd85fda660a66c74278399167c2921c1
arsmysk <11729510+arisamiyasaka@users.noreply.github.com>
2017-11-24 20:25:12 +0900

    add Todo and TodoList component

---
+++ src/components/Todo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default ({ attr: { id, completed, description }, $parent }) => {
+  return (
+    <li
+      style={{ textDecoration: completed ? 'line-through' : 'none' }}
+      onClick={() => $parent(id)}
+    >
+      {description}
+    </li>
+  )
+}
