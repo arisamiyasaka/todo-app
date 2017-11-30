7166a1156a9c54f02da0bf208665c8f91cf406e5
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-11-30 14:10:45 +0900

    chore: beautify some code

--- src/store.js
+++ src/store.js
@@ -2,17 +2,14 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 
 /* Logging middleware */
-const addLoggingToDispatch = (store) => (rawDispatch) => {
-  // New dispatch function
-  return (action) => {
-    console.group(action.type)
-    console.log('%c prev state', 'color: gray', store.getState())
-    console.log('%c action', 'color: blue', action)
-    const returnValue = rawDispatch(action)
-    console.log('%c next state', 'color: green', store.getState())
-    console.groupEnd(action.type)
-    return returnValue
-  }
+const addLoggingToDispatch = (store) => (rawDispatch) => (action) => {
+  console.group(action.type)
+  console.log('%c prev state', 'color: gray', store.getState())
+  console.log('%c action', 'color: blue', action)
+  const returnValue = rawDispatch(action)
+  console.log('%c next state', 'color: green', store.getState())
+  console.groupEnd(action.type)
+  return returnValue
 }
 
 /* recognizing Promise object middleware */
