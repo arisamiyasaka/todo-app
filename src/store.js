a08b987e31f1cb97341d0986e295c2cb1ed858d1
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-11-30 15:10:12 +0900

    Replace middlewares to npm published packages

--- src/store.js
+++ src/store.js
@@ -2,28 +2,13 @@ import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
 
 /* Logging middleware */
-const addLoggingToDispatch = (store) => (rawDispatch) => (action) => {
-  console.group(action.type)
-  console.log('%c prev state', 'color: gray', store.getState())
-  console.log('%c action', 'color: blue', action)
-  const returnValue = rawDispatch(action)
-  console.log('%c next state', 'color: green', store.getState())
-  console.groupEnd(action.type)
-  return returnValue
-}
+import addLoggingToDispatch from 'redux-logger'
 
 /* recognizing Promise object middleware */
-const addPromiseSupportToDispatch = (store) => (rawDispatch) => (action) => {
-  if (typeof action.then === 'function') {
-    return action.then(rawDispatch)
-  } else {
-    return rawDispatch(action)
-  }
-}
+import addPromiseSupportToDispatch from 'redux-promise'
 
 /* thunk middleware */
-const thunk = (store) => (next) => (action) =>
-  typeof action === 'function' ? action(store.dispatch) : next(action)
+import thunk from 'redux-thunk'
 
 const middlewares = [thunk, addPromiseSupportToDispatch, addLoggingToDispatch]
 
