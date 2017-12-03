c053e847a4cc1f84e1669b22331c40823156cad8
sundaycrafts <sundaycrafts@users.noreply.github.com>
2017-12-04 00:52:14 +0900

    Install React-router

--- src/index.js
+++ src/index.js
@@ -4,13 +4,26 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store'
 import {Provider} from 'react-redux'
+import Home from './components/Home'
+import Sub from './components/Sub'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 /** MEMO: 子コンポーネントで connect() を使用するために、ルートコンポーネントを <Provider> で包む */
 /** MEMO: react-redux をインストールしたことで、 <Provider> 以外が store を明示的に参照しなくなった */
 ReactDOM.render((
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <div>
+    <Provider store={store}>
+      <App />
+    </Provider>
+    <Router>
+      <div>
+        <Route exact path='/' component={Home} />
+        <Route path='/sub' component={Sub} />
+        <Link to={''}>Home</Link>
+        <Link to={'sub'}>Sub</Link>
+      </div>
+    </Router>
+  </div>
 ), document.getElementById('root'));
 
 /** MEMO: store.subscribe() は <Provider> が実行してくれるため不要になります。 */
